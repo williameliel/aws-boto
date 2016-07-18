ab4323ea226dbf81bf2cc30721e844b0c7100c35
williameliel <williameliel@gmail.com>
2016-07-18 12:02:52 -0400

    sql1

--- boto.py
+++ boto.py
@@ -1,9 +1,11 @@
+#import required modules
 import boto3
-import sh
-from sh import rsync
+import os
 
+#create a boto ec2 client
 client = boto3.client('ec2')
 
+#get all instances that are tagged under the sbe-auto-group
 response = client.describe_instances(
   Filters=[
         {
@@ -14,15 +16,21 @@ response = client.describe_instances(
             'Name': 'tag-value',
             'Values': [
                         'sbe-auto-group'
-                      ],
+                      ]
+           
         },
     ]
 )
 
-#print(response['Reservations'][0]['Instances'])
-for instance in response['Reservations'][0]['Instances']:
-  privateIp= instance['PrivateIpAddress']
-  publicIp = instance['PublicIpAddress']
-  print(publicIp, privateIp)
-  #rsync -rave "ssh -i /Users/Williamgarcia/.ssh/aws1.pem" ubuntu@52.33.127.143:/var/www/vhost2/rainbowroom/dev/web/content/uploads /Users/Williamgarcia/websites/rainbowroom/web/content
+
+destination = '/var/www/vhosts'
+source = destination  + '/sbe.com'
+
+for reservations in response['Reservations']:
+  for instance in reservations['Instances']:
+    if(instance['State']['Name']=="running"):
+      privateIp = instance['PrivateIpAddress']
+      publicIp = instance['PublicIpAddress']
+      #print(publicIp)
+      os.system("rsync --exclude-from exclude.txt -rave 'ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null  -i /home/ubuntu/.ssh/aws1.pem' " + source + ' ' +  'ubuntu@' + publicIp + ':' + destination )
 
